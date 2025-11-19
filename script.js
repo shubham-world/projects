4e3ed46c5806b49c07802d754ef9df2c8ba123f9
shubham-world <shubhamsh906@gmail.com>
2025-11-19 12:24:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("myButton");
+    button.addEventListener("click", function() {
+        
+        alert("You did it! The button was clicked.");
+    });
+
+});
