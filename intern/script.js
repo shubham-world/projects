17c275aa3b2f717fe361608bbcdd85112a3ce9c9
shubham-world <shubhamsh906@gmail.com>
2025-11-19 12:27:57 +0530

    Delete intern directory

--- intern/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("myButton");
-    button.addEventListener("click", function() {
-        
-        alert("You did it! The button was clicked.");
-    });
-
-});
